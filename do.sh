712b042559070b19613186f19192680624ac44e8
22i <pumpkin650@gmail.com>
2020-07-05 19:01:01 +0200

    Update do.sh

--- do.sh
+++ do.sh
@@ -6,6 +6,8 @@ mkdir --parents $HOME/.local/share/plasma/desktoptheme/
 # go inside
 cd $HOME/.local/share/plasma/desktoptheme/
 
+rm -f -r $HOME/.local/share/plasma/desktoptheme/Fantasy-Blue
+
 # put fantasy blue destop theme here
 git clone https://github.com/L4ki/Fantasy-Blue
 
@@ -19,6 +21,8 @@ mkdir --parents $HOME/.local/share/color-schemes/
 # go inside
 cd $HOME/.local/share/color-schemes/
 
+rm -f -r $HOME/.local/share/color-schemes/FantasyBlue.colors
+
 # put fantasy blue colorscheme here
 curl https://raw.githubusercontent.com/22i/customizekdescript/master/FantasyBlue.colors > FantasyBlue.colors
 
@@ -31,13 +35,16 @@ mkdir --parents $HOME/.local/share/plasma/look-and-feel/
 # go inside
 cd $HOME/.local/share/plasma/look-and-feel/
 
-
-
+rm -f -r $HOME/.local/share/plasma/look-and-feel/blue22i
 
 # put blue22i global theme into there
 git clone https://github.com/22i/blue22i
 
+
+
 # sets this as look and feel or global theme
 # lookandfeeltool -a 'org.kde.breeze.desktop
+
+lookandfeeltool -a blue22i
 
 echo done everything
